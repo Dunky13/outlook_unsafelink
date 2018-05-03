6a1293c24db657c53db0874484a3b88eaa3d1f3c
Dunky13 <mwent@live.nl>
2018-05-03 14:12:19 +0200

    Rerun replace every 150ms

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
-var urls = document.getElementsByTagName('a');
-for (var i = 0, l = urls.length; i < l; i++) {
-  var dec = decodeURIComponent(urls[i].href);
-  if (dec.indexOf('safelinks.protection.outlook.com') > 0) {
-    urls[i].href = dec.substring(dec.indexOf("?url=")+5,dec.indexOf("&data="));
+function replaceUrls() {
+  var urls = [...document.getElementsByTagName('a')].filter(x => x.href.indexOf('safelinks.protection.outlook.com') > 0)
+  for (var i = 0, l = urls.length; i < l; i++) {
+    var href_dec = decodeURIComponent(urls[i].href);
+    urls[i].href = href_dec.substring(href_dec.indexOf("?url=")+5,href_dec.indexOf("&data="));
+    if(urls[i].innerText.indexOf('safelinks.protection.outlook.com') > 0) {
+      var inner_dec = decodeURIComponent(urls[i].innerText);
+      urls[i].innerText = inner_dec.substring(inner_dec.indexOf("?url=")+5,inner_dec.indexOf("&data="));
+    }
   }
-  var dec = decodeURIComponent(urls[i].innerText);
-  if (dec.indexOf('safelinks.protection.outlook.com') > 0) {
-    urls[i].innerText = dec.substring(dec.indexOf("?url=")+5,dec.indexOf("&data="));
-  }
+  setTimeout(replaceUrls, 150);
 }
+replaceUrls();
